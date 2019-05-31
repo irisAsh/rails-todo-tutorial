1f92a9afd2847d96c7e385cc0c500e5d0f3d93a5
irisAsh <043takotoka@gmail.com>
2019-06-01 00:30:47 +0900

    Setup session function

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    registrations: 'users/registrations'
+    registrations: "users/registrations",
+    sessions: "users/sessions"
   }
   root "welcome#index"
   get "welcome/index"
